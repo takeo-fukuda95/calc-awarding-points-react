e7d7b3b5fe76c0d4ebc4171dc87a14d400ca63bd
Takeo Fukuda <takeo.fukuda95@gmail.com>
2022-10-03 23:26:59 +0900

    Completed the logic to calculate the total and monthly poins based on the transaction history.

--- src/features/award/Award.js
+++ src/features/award/Award.js
@@ -1,24 +1,75 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useCallback, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import CircularProgress from '@mui/material/CircularProgress';
+import debounce from 'lodash/debounce';
+import {
+  fetchCustomers,
+  fetchTransactions,
+  currentCustomers,
+  currentTransactions,
+  currentCustomersStatus
+} from './awardSlice';
+import { calcTotalPoints, calcMonthlyPoints } from './awardFunction';
 import styles from './Award.module.css';
 
+const DEBOUNCE_DELAY = 1500;
+
 const Award = () => {
+  const customerSuggestions = useSelector(currentCustomers);
+  const transactions = useSelector(currentTransactions);
+  const fetchCustomersStatus = useSelector(currentCustomersStatus);
+  const dispatch = useDispatch();
+
   const [open, setOpen] = useState(false);
   const [isLoadingCustomers, setIsLoadingCustomers] = useState(false);
-  const [customerSuggestions, setCustomerSuggestions] = useState([]);
+
+  const fetchCustomerQuery = useCallback(
+    val => {
+      setIsLoadingCustomers(true);
+      dispatch(fetchCustomers(val));
+    },
+    [dispatch]
+  );
+
+  const fetchTransactionQuery = useCallback(
+    val => {
+      setIsLoadingCustomers(true);
+      dispatch(fetchTransactions(val));
+    },
+    [dispatch]
+  );
+
+  const delayedCustomerQuery = useMemo(
+    () => 
+      debounce(val => {
+        fetchCustomerQuery(val);
+      }, DEBOUNCE_DELAY),
+    [fetchCustomerQuery]
+  );
   
   useEffect(() => {
-    setIsLoadingCustomers(open);
-  }, [open]);
+    setIsLoadingCustomers((open) || fetchCustomersStatus === 'loading');
+    
+    return () => {
+      delayedCustomerQuery.cancel();
+    };
+  }, [open, customerSuggestions, fetchCustomersStatus, delayedCustomerQuery]);
   
   const getCustomerSuggestionsOnValue = (keyword) => {
+    delayedCustomerQuery(keyword);
+  }
+
+  const resetCustomerSuggestionsOnValue = () => {
+    fetchTransactionQuery(0);
   }
 
   const onChangeCustomer = (customerInfo) => {
     if (!customerInfo)
       return;
+
+    fetchTransactionQuery(customerInfo.id);
   }
 
   return (
@@ -41,6 +92,8 @@ const Award = () => {
         onInputChange={(event, value, reason) => {
           if (reason === 'input') {
             getCustomerSuggestionsOnValue(value);
+          } else if (reason === 'clear') {
+            resetCustomerSuggestionsOnValue();
           }
         }}
         onChange={(event, value, reason, details) => onChangeCustomer(value)}
@@ -60,6 +113,52 @@ const Award = () => {
           />
         )}
       />
+      {transactions.length > 0 && (
+        <div className={styles.infoContainer}>
+          <div className={styles.transactionsContainer}>
+            <div className={styles.transactionsContainerHeader}>
+              <span>Amount</span>
+              <span>Trasaction Date</span>
+            </div>
+            <div className={styles.transactionsContainerBody}>
+              {transactions.map((each, index) => {
+                const eachDateTime = new Date(each.time);
+                return (
+                  <div key={index}>
+                    <span>${each.amount}</span>
+                    <span>{`${eachDateTime.getMonth() + 1}/${eachDateTime.getDate()} ${eachDateTime.getFullYear()}`}</span>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+          <div className={styles.pointsContainer}>
+            <div className={styles.totalPointsContainer}>
+              Total Points: {calcTotalPoints(transactions)}
+            </div>
+            <div className={styles.monthlyPointsContainer}>
+              <div className={styles.monthlyPointsContainerHeader}>
+                <div className={styles.monthlyPointsContainerHeaderTitle}>Montly Points:</div>
+                <div className={styles.monthlyPointsContainerHeaderCaption}>
+                  <span>Month</span>
+                  <span>Awarding Points</span>
+                </div>
+              </div>
+              <div className={styles.monthlyPointsContainerBody}>
+                {Object.entries(calcMonthlyPoints(transactions)).map(([key, value], i) => {
+                    return (
+                      <div key={i} className={styles.eachMonthPointsContainer}>
+                        <span>{key}</span>
+                        <span>{value} points</span>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
